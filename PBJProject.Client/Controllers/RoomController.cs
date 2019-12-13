f35f8de209a31fe767e90006b025e0c96b040738
Phillip Krawetz <phillip.kraw@gmail.com>
2019-12-13 14:57:42 -0600

    update room controller and chat view

---
+++ PBJProject.Client/Controllers/RoomController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PBJProject.Client.Models;
+
+namespace PBJProject.Client.Controllers
+{
+  public class RoomController : Controller
+  {
+    public ActionResult Index()
+    {
+      if(HttpContext.Session.GetString("name") == null)
+      {
+        return Redirect("/");
+      }
+
+      return RedirectToAction("Chat");
+    }
+
+    public ActionResult Chat()
+    {
+      return View();
+    }
+  }
+}
