1d0bf9f2e8e60fdf0f26d3ef98f5d39fbe6e7222
Benjamin Clegg <b.clegg>
2019-12-14 20:50:15 -0600

    Database stuff

--- PBJProject.Domain/Models/Character.cs
+++ PBJProject.Domain/Models/Character.cs
@@ -2,8 +2,10 @@ namespace PBJProject.Domain.Models
 {
   public class Character
   {
+    public int ID { get; set; }
     public int Level { get; set; }
     public string Name { get; set; }
+    public string Race { get; set; }
     public int Strength { get; set; }
     public int Intelligence { get; set; }
     public int Dexterity { get; set; }
@@ -11,6 +13,9 @@ namespace PBJProject.Domain.Models
     public int Charisma { get; set; }
     public int Constitution { get; set; }
     public string CharacterClass { get; set; }
+    public int AccountID { get; set; }
     public string FileName { get; set; }
+
+    public virtual Account Account { get; set; }
   }
 }
--- PBJProject.Storing/PbjDbContext.cs
+++ PBJProject.Storing/PbjDbContext.cs
@@ -7,40 +7,64 @@ namespace PBJProject.Domain
   public class PbjDbContext : DbContext
   {
     public DbSet<Account> Account { get; set; }
+    public DbSet<Character> Character { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
     {
-      dbContext.UseNpgsql("server=localhost;database=postgres;user id=postgres;password=postgres");
+      dbContext.UseNpgsql("server=localhost;database=pbjproject;user id=postgres;password=postgres");
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-      builder.Entity<Account>(o => o.HasKey(k => k.Id));
-      builder.Entity<Account>().Property(p => p.Id).UseSerialColumn();
+      builder.Entity<Account>(o => o.HasKey(k => k.ID));
+      builder.Entity<Account>().Property(p => p.ID)
+                     .UseSerialColumn();
 
       builder.Entity<Account>().HasData(new List<Account>()
       {
-        new Account(){ UserName = "jguillo",
+        new Account(){ ID = 1,
+                       UserName = "jguillo",
                        FirstName = "Joshua",
                        LastName = "Guillory",
                        Password = "revature7",
                        Email = "joshua@familiar.com" },
-        new Account(){ UserName = "phillip",
+        new Account(){ ID = 2,
+                       UserName = "phillip",
                        FirstName = "Phillip",
                        LastName = "Krawetz",
                        Password = "revature8",
                        Email = "phillip@familiar.com" },
-        new Account(){ UserName = "sven",
+        new Account(){ ID = 3,
+                       UserName = "sven",
                        FirstName = "Benjamin",
                        LastName = "Clegg",
                        Password = "revature9",
                        Email = "benjamin@familiar.com" },
-        new Account(){ UserName = "lisa",
+        new Account(){ ID = 4,
+                       UserName = "lisa",
                        FirstName = "Phillip",
                        LastName = "Krawetz",
                        Password = "revature8",
                        Email = "phillip@familiar.com" }
       });
+
+      builder.Entity<Character>(o => o.HasKey(k => k.ID));
+      builder.Entity<Character>().Property(p => p.ID).UseSerialColumn();
+
+      builder.Entity<Character>().HasData(new List<Character>()
+      {
+         new Character(){ ID = 1,
+                          Level = 1,
+                          Name = "Dummy",
+                          Race = "Human",
+                          Strength = 18,
+                          Intelligence = 8,
+                          Dexterity = 12,
+                          Wisdom = 10,
+                          Charisma = 10,
+                          Constitution = 16,
+                          CharacterClass = "Fighter" }
+         });
+      }
     }
-  }
 }
