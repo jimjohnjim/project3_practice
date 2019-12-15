db97688c61f08aa997a1d12c9d8e81f17715fb41
Benjamin Clegg <b.clegg>
2019-12-14 18:24:38 -0600

    Added Dice.cs unit tests

--- PBJProject.Testing/Specs/PBJProject.Domain/Models/DiceTest.cs
+++ PBJProject.Testing/Specs/PBJProject.Domain/Models/DiceTest.cs
@@ -5,7 +5,9 @@ namespace PBJProject.Testing.Specs.PBJProject.Domain.Models
 {
    public class DiceTest
    {
+      private const int FourDice = 4;
       private const int ThreeDice = 3;
+      private const int TwoDice = 2;
       private const int SixSided = 6;
 
       [Fact]
@@ -21,20 +23,77 @@ namespace PBJProject.Testing.Specs.PBJProject.Domain.Models
          Assert.NotEmpty(sut.Values);
       }
 
-      [Fact]
-      public void Test_Dice_Roll_HasValuesInRange()
+      [Theory]
+      [InlineData(ThreeDice, SixSided)]
+      public void Test_Dice_Roll_HasValuesInRange(int numDice, int sidesDice)
       {
          // arrange
-         var sut = new Dice(ThreeDice, SixSided);
+         var sut = new Dice(numDice, sidesDice);
 
          // act
          sut.Roll();
 
          // assert
-         Assert.Collection(sut.Values, value => Assert.InRange(value, 1, 6),
-                                       value => Assert.InRange(value, 1, 6),
-                                       value => Assert.InRange(value, 1, 6)
+         Assert.Collection(sut.Values, value => Assert.InRange(value, 1, sidesDice),
+                                       value => Assert.InRange(value, 1, sidesDice),
+                                       value => Assert.InRange(value, 1, sidesDice)
          );
+      }
+
+      [Theory]
+      [InlineData(new int[] {1, 2, 3})]
+      public void Test_Dice_Sum(int[] values)
+      {
+         // arrange
+         var sut = new Dice(ThreeDice, SixSided);
+
+         // act
+         sut.Values.AddRange(values);
+
+         // assert
+         Assert.Equal(sut.Sum, 6);
+      }
+
+      [Theory]
+      [InlineData(new int[] {1, 2, 3})]
+      public void Test_Dice_Highest(int[] values)
+      {
+         // arrange
+         var sut = new Dice(ThreeDice, SixSided);
+
+         // act
+         sut.Values.AddRange(values);
+
+         // assert
+         Assert.Equal(sut.Highest, 3);
+      }
+
+      [Theory]
+      [InlineData(ThreeDice)]
+      public void Test_Dice_RemoveOneDieFromPool(int numDice)
+      {
+         // arrange
+         var sut = new Dice(numDice, SixSided);
+
+         // act
+         sut.RemoveOneDieFromPool();
+
+         // assert
+         Assert.Equal(sut.NumberOfDice, 2);
+      }
+
+      [Theory]
+      [InlineData(ThreeDice)]
+      public void Test_Dice_AddOneDieToPool(int numDice)
+      {
+         // arrange
+         var sut = new Dice(numDice, SixSided);
+
+         // act
+         sut.AddOneDieToPool();
+
+         // assert
+         Assert.Equal(sut.NumberOfDice, 4);
       }
    }
 }
