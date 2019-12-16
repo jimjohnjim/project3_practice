5196291bb43ba3f299e7702a2ff4a61eea453d97
Phillip Krawetz <phillip.kraw@gmail.com>
2019-12-15 18:37:00 -0600

    add dice roll message

--- PBJProject.Client/wwwroot/js/chat.js
+++ PBJProject.Client/wwwroot/js/chat.js
@@ -43,6 +43,33 @@ document.getElementById("messageInput").addEventListener("keyup", function (even
   event.preventDefault();
 });
 
+connection.on("DiceRoll", function(user,dice,sum,highest) {
+  var message = user + " rolled";
+  var i;
+
+  for(i = 0; i < dice.length; i++)
+  {
+    message += " " + dice[i];
+    if(i!=dice.length-1){
+      message += ",";
+    }
+  }
+
+  var encodedMsg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  var li = document.createElement("li");
+  li.textContent = encodedMsg;
+  document.getElementById("messagesList").appendChild(li);
+  if(dice.length > 1){
+    var liSum = document.createElement("li");
+    var liHigh = document.createElement("li");
+    liSum.textContent = "Total: " + sum;
+    document.getElementById("messagesList").appendChild(liSum);
+    liHigh.textContent = "Highest: " + highest;
+    document.getElementById("messagesList").appendChild(liHigh);
+  }
+  
+})
+
 connection.on("UserJoined", function (user, users) {
   var message = user + " has joined the party!";
   var encodedMsg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
