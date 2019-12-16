9a608ec7de49c72213fc1b3d720c15f72dff2b57
Benjamin Clegg <b.clegg>
2019-12-15 23:38:25 -0600

    Added more tests

--- PBJProject.Testing/Specs/PBJProject.Storing/Adapters/JSONAdapterTest.cs
+++ PBJProject.Testing/Specs/PBJProject.Storing/Adapters/JSONAdapterTest.cs
@@ -1,3 +1,4 @@
+using PBJProject.Domain.Models;
 using PBJProject.Storing.Adapters;
 using Xunit;
 
@@ -6,7 +7,7 @@ namespace PBJProject.Testing.Specs.PBJProject.Storing.Adapters
    public class JSONAdapterTest
    {
       [Fact]
-      public void Test_ValidateJSON()
+      public void Test_ValidateJSONWithBadData()
       {
          // arrange
          var sut = new JSONAdapter();
@@ -24,6 +25,79 @@ namespace PBJProject.Testing.Specs.PBJProject.Storing.Adapters
 
          // assert
          Assert.False(actual);
+      }
+
+      [Fact]
+      public void Test_ValidateJSONWithGoodData()
+      {
+         //arrange
+         var sut = new JSONAdapter();
+         var schema = new JSONSchemas();
+         var testString = @"{
+            'Name': 'Geralt of Rivia',
+            'Race': 'Mutant',
+            'CharacterClass': 'Witcher',
+            'Level': 100,
+            'Strength': 18,
+            'Intelligence': 16,
+            'Dexterity': 22,
+            'Wisdom': 16,
+            'Charisma': 12,
+            'Constitution': 18
+         }";
+
+         // act
+         var actual = sut.ValidateJSON(testString, schema.SchemaDnD);
+
+         // assert
+         Assert.True(actual);
+      }
+
+      [Fact]
+      public void Test_LoadWithBadDataReturnsNull()
+      {
+         // arrange
+         var sut = new JSONAdapter();
+         var schema = new JSONSchemas();
+         var testString = @"{
+               CPU: 'Intel',
+               Drives: [
+                  'DVD read/writer',
+                  '500 gigabyte hard drive'
+               ]
+         }";
+
+         // act
+         var actual = sut.Load(testString);
+
+         // assert
+         Assert.Null(actual);
+      }
+
+      [Fact]
+      public void Test_LoadWithGoodDataReturnsCharacter()
+      {
+         // arrange
+         var sut = new JSONAdapter();
+         var schema = new JSONSchemas();
+         var testString = @"{
+            'Name': 'Geralt of Rivia',
+            'Race': 'Mutant',
+            'CharacterClass': 'Witcher',
+            'Level': 100,
+            'Strength': 18,
+            'Intelligence': 16,
+            'Dexterity': 22,
+            'Wisdom': 16,
+            'Charisma': 12,
+            'Constitution': 18
+         }";
+
+         // act
+         var actual = sut.Load(testString);
+
+         // assert
+         Assert.IsType<Character>(actual);
       }
    }
 }
